666717513c688181562a722e26d03707cc2b8f8d
kailash-anand <kailash.anand@stonybrook.edu>
2025-04-24 01:23:32 -0400

    Parsing and file read

---
+++ implant363/tmp363.py
@@ -0,0 +1,34 @@
+import argparse
+from scan_files import scan_device
+
+server_IP = None
+server_port = None
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("target", type=str)
+    parser.add_argument("port", type=int)
+
+    args = parser.parse_args()
+
+    global server_IP, server_port
+
+    server_IP = args.target
+    server_port = args.port
+
+def main():
+    global server_IP, server_port
+
+    parse_args()
+    
+    if server_IP == None or server_port == None:
+        return 
+    
+    if server_port < 0 or server_port > 65535:
+        return   
+    
+    scan_device()
+
+if __name__ == "__main__":
+    main()
